b3c33a0af06b1320aea3c439b50c2d6808b26cf0
Amsavarthani Selvaraj <s.amsavarthani@gmail.com>
2025-03-11 12:37:33 +0530

    some changes

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function sortString(order) {
         let inputElement = document.getElementById("input");
         let outputElement = document.getElementById("para");
 
-        let words = inputElement.value.trim().split(" "); 
+        let words = inputElement.value.trim().split(/\s+/); 
         
-        if (words.length !== 2) {
-            outputElement.innerText = "Please enter exactly two words!";
+        if (words.length < 2) {
+            outputElement.innerText = "Please enter atleast two words!";
             return;
         }
         words.sort((a, b) => a[0].localeCompare(b[0]));
