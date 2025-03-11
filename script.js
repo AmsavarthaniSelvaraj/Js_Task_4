f1d97b3d537b95978eb178be978d02f08799f616
Amsavarthani Selvaraj <s.amsavarthani@gmail.com>
2025-03-11 12:32:09 +0530

    second commit

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ function sortString(order) {
         let inputElement = document.getElementById("input");
         let outputElement = document.getElementById("para");
 
-        let words = inputElement.value.trim().split(" "); // Split input into words
+        let words = inputElement.value.trim().split(" "); 
         
         if (words.length !== 2) {
             outputElement.innerText = "Please enter exactly two words!";
             return;
         }
-
-        // Sorting based on first letter
         words.sort((a, b) => a[0].localeCompare(b[0]));
 
         if (order === 'desc') {
